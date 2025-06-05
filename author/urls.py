8a2296fac5ecf333894e6b15fad5b50a2acb6091
Max <smolinskijmaksim1@gmail.com>
2025-06-05 11:36:45 +0100

    Add basename in router.register

--- author/urls.py
+++ author/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from author.views import AuthorViewSet
 
 router = routers.SimpleRouter()
-router.register("author", AuthorViewSet)
+router.register("author", AuthorViewSet, basename="manage")
 
 urlpatterns = [
     path("", include(router.urls))
